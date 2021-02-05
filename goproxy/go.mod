20df0d596dac862b1434c42c84529f3c8162c8a7
ezware <ezware@163.com>
2021-02-05 11:16:22 +0800

    1. Add goproxy 2. add githttp.service 	new file:   githttp/githttp.service 	new file:   goproxy/go.mod 	new file:   goproxy/goproxy.service 	new file:   goproxy/proxy.go

---
+++ goproxy/go.mod
@@ -0,0 +1,5 @@
+module github.com/ezware/goproxy
+
+go 1.14
+
+require github.com/goproxy/goproxy v0.2.2
---
+++ goproxy/proxy.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/goproxy/goproxy"
+	"net/http"
+	"os"
+)
+
+var (
+	rootdir  string
+	httpport int
+)
+
+func init() {
+	flag.StringVar(&rootdir, "gopath", "/www/goproxy", "specify the go path to store downloaded file")
+	flag.IntVar(&httpport, "port", 8100, "specify the goproxy listen port")
+}
+
+func main() {
+	flag.Parse()
+
+	gopathEnv := "GOPATH=" + rootdir
+	listenAddr := fmt.Sprintf(":%d", httpport)
+
+	g := goproxy.New()
+	g.GoBinEnv = append(
+		os.Environ(),
+		"GOPROXY=https://goproxy.cn,direct", // Use goproxy.cn as the upstream proxy
+		gopathEnv,
+		//		"GOPRIVATE=git.example.com",         // Solve the problem of pulling private modules
+	)
+	g.ProxiedSUMDBs = []string{"sum.golang.org https://goproxy.cn/sumdb/sum.golang.org"} // Proxy the default checksum database
+
+	fmt.Printf("Listenning on \"%s\", %s\n", listenAddr, gopathEnv)
+
+	http.ListenAndServe(listenAddr, g)
+}
